8c881eb0f7f48eabdcf5573f0793ea091e75015b
NicholasMcManus <mcmanusnj3@gmail.com>
2018-10-18 10:17:50 -0400

    Created Netbeans Javafx Project

---
+++ CardGame/src/cardgame/CardGame.java
@@ -0,0 +1,54 @@
+/*
+ * file name: CardGame.java
+ * programmer name: Nick McManus
+ * date created: 10-18-2018
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+package cardgame;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Nick McManus
+ */
+public class CardGame extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
