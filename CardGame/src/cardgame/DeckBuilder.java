fc3d248af5e1ccad50b1454ebe9a4870fb88f3ce
NicholasMcManus <mcmanusnj3@gmail.com>
2018-11-16 18:22:55 -0500

    Still unstable when trying to call getFile

--- CardGame/src/cardgame/DeckBuilder.java
+++ CardGame/src/cardgame/DeckBuilder.java
@@ -2,20 +2,33 @@
  * file name: DeckBuilder.java
  * programmer name: Nick McManus
  * date created: 11-15-2018
- * date of last revision: 
- * details of last revision:
- * short description: 
+ * date of last revision: 11-15-2018
+ * details of last revision: Add methods for getting the front and back of cards
+ * short description: Be able to construct a deck with a certain number of
+                        cards
  */
 
 package cardgame;
 
+import java.io.File;
+
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 public class DeckBuilder {
     private final int CARDS_REQUIRED;
     private Deck myDeck;
+    private ImageView deckBack = null;
+    
+    //Keep track of cards in Deck until a
+    //method is added to Deck
+    int cardsInDeck = 0;
     
     public DeckBuilder(int cardsRequired)
     {
         CARDS_REQUIRED = cardsRequired;
+        myDeck = new Deck();
     }
     
     public DeckBuilder()
@@ -23,5 +36,54 @@ public class DeckBuilder {
         this(52);
     }
     
+    public void buildDeck()
+    {
+        ImageView cardFront;
+        while(deckBack == null)
+        {
+            getDeckBack();
+        }
+        
+        for(int i = 0; i < CARDS_REQUIRED; i++)
+        {
+            do
+            {
+                cardFront = getCardFront();
+            }while(cardFront == null);
+            
+            //Create a card with front and back images
+            
+            //Add the card to the deck
+            cardsInDeck++;
+        }
+    }
     
+    private boolean getDeckBack()
+    {
+        deckBack = new ImageView(getFile().getPath());
+        return deckBack == null;
+    }
+    
+    private ImageView getCardFront()
+    {
+        ImageView cardFront = new ImageView(getFile().getPath());
+        return cardFront;
+    }
+    
+    //Still doesn't work right, More testing is required
+    //*/
+    private File getFile()
+    {
+        Stage mainStage = new Stage();
+        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter("Image Files", "*.png",
+                    "*.jpg", "*.gif"));
+        File selectedFile = fileChooser.showOpenDialog(mainStage);
+        
+        return selectedFile;
+    }
+    //*/
 }
