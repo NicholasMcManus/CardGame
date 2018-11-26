729a3e8064e9affed29af9919f434dedff2f88e2
NicholasMcManus <mcmanusnj3@gmail.com>
2018-11-26 16:04:03 -0500

    Still unstable when trying to call getFile

--- CardGame/src/cardgame/DeckBuilder.java
+++ CardGame/src/cardgame/DeckBuilder.java
@@ -13,13 +13,20 @@ package cardgame;
 import java.io.File;
 
 import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DeckBuilder {
     private final int CARDS_REQUIRED;
     private Deck myDeck;
     private ImageView deckBack = null;
+    private final FileChooser fileChooser = new FileChooser();
     
     //Keep track of cards in Deck until a
     //method is added to Deck
@@ -36,19 +43,19 @@ public class DeckBuilder {
         this(52);
     }
     
-    public void buildDeck()
+    public void buildDeck(Stage stage)
     {
         ImageView cardFront;
         while(deckBack == null)
         {
-            getDeckBack();
+            getDeckBack(stage);
         }
         
         for(int i = 0; i < CARDS_REQUIRED; i++)
         {
             do
             {
-                cardFront = getCardFront();
+                cardFront = getCardFront(stage);
             }while(cardFront == null);
             
             //Create a card with front and back images
@@ -58,26 +65,37 @@ public class DeckBuilder {
         }
     }
     
-    private boolean getDeckBack()
+    private boolean getDeckBack(Stage stage)
     {
-        deckBack = new ImageView(getFile().getPath());
+        fileChooser.setTitle("Back of Card");
+        File backOfCard = fileChooser.showOpenDialog(stage);
+        Image cardBack;
+        
+        try {
+            cardBack = new Image(new FileInputStream(backOfCard));
+        } catch (FileNotFoundException ex) {
+            System.out.println("The back of the file could not be read.");
+            return false;
+        }
+        
+        deckBack = new ImageView(cardBack);
         return deckBack == null;
     }
     
-    private ImageView getCardFront()
+    private ImageView getCardFront(Stage stage)
     {
-        ImageView cardFront = new ImageView(getFile().getPath());
+        ImageView cardFront = new ImageView(getFile(stage).getPath());
         return cardFront;
     }
     
     //Still doesn't work right, More testing is required
     //*/
-    private File getFile()
+    private File getFile(Stage mainStage)
     {
-        Stage mainStage = new Stage();
+        //Stage mainStage = new Stage();
         
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
+        //FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Picture");
         fileChooser.getExtensionFilters().add(
             new FileChooser.ExtensionFilter("Image Files", "*.png",
                     "*.jpg", "*.gif"));
