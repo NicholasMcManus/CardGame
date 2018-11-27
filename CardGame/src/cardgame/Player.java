c22c643d6460985d748b30a0264cd0f89d78991a
NicholasMcManus <mcmanusnj3@gmail.com>
2018-11-27 09:29:57 -0500

    Add JavaDoc comments

--- CardGame/src/cardgame/Player.java
+++ CardGame/src/cardgame/Player.java
@@ -2,8 +2,8 @@
  * file name: Player.java
  * programmer name: Nick McManus
  * date created: 11-08-2018
- * date of last revision: 11-15-2018
- * details of last revision: Finish writing header line
+ * date of last revision: 11-27-2018
+ * details of last revision: Write JavaDoc Comments
  * short description: Describe a player object to contain scores and a name
  */
 
@@ -16,21 +16,37 @@ public class Player implements java.io.Serializable{
     private ArrayList<Score> scoreList;
     private String playerName;
     
+    /**
+     * Default constructor for the Player Class
+     */
     public Player(){
         this("Potato");
     }
     
+    /**
+     * Constructor allowing for a player name to be set on initialization
+     * @param playerName The name for the player
+     */
     public Player(String playerName){
         this.playerName = playerName;
         scoreList = new ArrayList<>();
     }
     
+    /**
+     * Constructor allowing for a player to be created with name and score
+     * @param playerName Name of the player
+     * @param score Score of the player
+     */
     public Player(String playerName, int score)
     {
         this(playerName);
         scoreList.add(new Score(score));
     }
     
+    /**
+     * Add a score to the player's list of scores
+     * @param score The score to be added
+     */
     public void addScore(int score){
         scoreList.add(new Score(score));
         Collections.sort(scoreList, Collections.reverseOrder());
@@ -40,6 +56,12 @@ public class Player implements java.io.Serializable{
         }
     }
     
+    /**
+     * Getter for a specific score from the player's list of scores
+     * @param scoreNumber The number of the score to retrieve
+     * @return A score object representing the score of a player
+     * @throws ArrayIndexOutOfBoundsException 
+     */
     public Score getScore(int scoreNumber) 
             throws ArrayIndexOutOfBoundsException{
         if(scoreNumber >= scoreList.size())
@@ -48,15 +70,27 @@ public class Player implements java.io.Serializable{
         return scoreList.get(scoreNumber);
     }
     
+    /**
+     * Get the number of scores stored in the player object
+     * @return The number of scores as an integer
+     */
     public int getNumScores()
     {
         return scoreList.size();
     }
     
+    /**
+     * Get the name of the player
+     * @return The name of the player
+     */
     public String getName(){
         return playerName;
     }
     
+    /**
+     * Allow the name of the player to be changed
+     * @param name The new name for the player
+     */
     public void setName(String name){
         this.playerName = name;
     }
