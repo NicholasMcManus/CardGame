fe0015c80ea076d7e9f3c724c3d3d5a90bfcedba
NicholasMcManus <mcmanusnj3@gmail.com>
2018-11-27 09:41:28 -0500

    Add JavaDoc comments

--- CardGame/src/cardgame/Scoreboard.java
+++ CardGame/src/cardgame/Scoreboard.java
@@ -2,8 +2,8 @@
  * file name: Scoreboard.java
  * programmer name: Nick McManus
  * date created: 11-12-2018
- * date of last revision: 11-13-2018
- * details of last revision: Make the scoreboard work
+ * date of last revision: 11-27-2018
+ * details of last revision: Add java documentation to the class
  * short description: Be able to create a scoreboard to display a number of
                         high scores
  */
@@ -18,6 +18,13 @@ public class Scoreboard {
     
     private final int NUM_SCORES;
     
+    /**
+     * Construct a scoreboard from a list of players and
+     * direct a number of scores
+     * @param players A list containing a number of players to be used to construct
+     * the list
+     * @param numScores The number of scores to keep on the scoreboard
+     */
     public Scoreboard(ArrayList<Player> players, int numScores)
     {
         NUM_SCORES = numScores;
@@ -27,11 +34,19 @@ public class Scoreboard {
         fillScoreBoard(players);
     }
     
+    /**
+     * A constructor to create a scoreboard with 10 scores and a list of players
+     * @param players The list of players to use when constructing a scoreboard
+     */
     public Scoreboard(ArrayList<Player> players)
     {
         this(players, 10);
     }
     
+    /**
+     * A method to actually fill the scoreboard with members
+     * @param players The list of players to add to the scoreboard
+     */
     private void fillScoreBoard(ArrayList<Player> players)
     {
         //For every player
@@ -55,6 +70,12 @@ public class Scoreboard {
         }
     }
     
+    /**
+     * Method to add scores to the two array lists
+     * @param playerName The name of the player to add
+     * @param score The score of the player to add
+     * @return 
+     */
     private boolean addScore(String playerName, Score score)
     {
         if(highScores.isEmpty())
@@ -85,11 +106,21 @@ public class Scoreboard {
         return false;
     }
 
+    /**
+     * Retrieve the number of scores stored in the scoreboard
+     * @return The number of scores on the scoreboard
+     */
     public int getNumScores()
     {
         return highScores.size();
     }
     
+    /**
+     * Get a name from the scoreboard at a specific index
+     * @param index The index to pull the name from
+     * @return The name of a player stored in a particular index
+     * @throws ArrayIndexOutOfBoundsException 
+     */
     public String getName(int index)
                 throws ArrayIndexOutOfBoundsException{
         if(index >= highScorePlayer.size())
@@ -98,6 +129,12 @@ public class Scoreboard {
         return highScorePlayer.get(index);
     }
     
+    /**
+     * Get a score from the scoreboard at a specific index
+     * @param scoreNumber The index to pull the score from
+     * @return The score of a player stored in a particular index
+     * @throws ArrayIndexOutOfBoundsException 
+     */
     public Score getScore(int scoreNumber) 
             throws ArrayIndexOutOfBoundsException{
         if(scoreNumber >= highScores.size())
