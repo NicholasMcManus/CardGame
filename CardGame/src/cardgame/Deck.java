0bfdd1c58e917532cb474a45a384eeb30952fc3e
NicholasMcManus <44237239+NicholasMcManus@users.noreply.github.com>
2018-12-12 18:30:43 -0500

    Update Deck.java

--- CardGame/src/cardgame/Deck.java
+++ CardGame/src/cardgame/Deck.java
@@ -1,8 +1,8 @@
 /*
  * file name: Deck.java
- * programmer name: Nick McManus
+ * programmer name: Josue, Helia, Nick
  * date created: 11-05-2018
- * date of last revision: 11-27-2018
+ * date of last revision: 12-12-2018
  * details of last revision: Add JavaDoc Comments
  * short description: Describe a collection of cards called a deck
  */
@@ -27,7 +27,6 @@ import javafx.scene.image.ImageView;
 public class Deck implements java.io.Serializable{
     //Declare Variables
     private ArrayList<Card> deck = new ArrayList();
-    //private final String defaultbackCard = "card/b1fv.png";
     
     /**
      * Default Constructor
@@ -53,6 +52,7 @@ public class Deck implements java.io.Serializable{
      */
     public boolean addCard(Card newCard)
     {
+        //Add a new card to the deck as long as it already doesn't exist in the deck
         if(!deck.contains(newCard))
         {
             deck.add(newCard);
@@ -61,8 +61,15 @@ public class Deck implements java.io.Serializable{
         return false;
     }
     
+    /**
+     * Add a card such that it can be used from the console window
+     * @param suit The suit of the card
+     * @param name The name for the card
+     * @param value The value of the card
+     */
     public void addCard(String suit, String name, int value)
     {
+        //Make sure the card does not already exist when adding it to the deck
         for (int i = 0; i < deck.size(); i++){
            if (deck.get(i).getSuit().equals(suit)
                && deck.get(i).getValue() == value
@@ -81,12 +88,13 @@ public class Deck implements java.io.Serializable{
      * @throws FileNotFoundException 
      */
     public void addCard(String cardFront, String cardBack) throws FileNotFoundException{
-
+        //Declare local variables
         int value; 
         String suit;
         
         value = Integer.parseInt(cardFront.substring(cardFront.indexOf(File.separatorChar)+1, cardFront.indexOf(".")));
         
+        //Determine the suit of the card
         if (value <= 13)
             suit = "spades";
         else if (value <= 26){
@@ -102,13 +110,13 @@ public class Deck implements java.io.Serializable{
             value -= 39;
         }
         
-       
+       //Add a new card with the values determined above
        deck.add(new Card(cardFront, cardBack, suit, value));
     }
     
     /**
      * Get the list of cards in the form of a deck
-     * @return 
+     * @return A reference to the list of cards
      */
     public ArrayList<Card> getDeck(){
         return deck;
@@ -137,21 +145,28 @@ public class Deck implements java.io.Serializable{
         inStream = new ObjectInputStream(new FileInputStream(fileName));
         
         this.deck = ((Deck)inStream.readObject()).getDeck();
-        
     }
     
+    /**
+     * Build a deck of cards given a certain filename
+     * @params folder The folder which contains at least 53 cards
+     */
     public void buildDeck(String folder) throws FileNotFoundException{
+        //Overwrite the current deck when called
         deck = new ArrayList();
         String fileName = "NULL", backName = "NULL";
         
+        //Try to read in 52 cards with backs
         for (int i = 1; i <= 52; i++){
             
             try{
+                //Generate the filenames
                 fileName = folder + File.separatorChar + i + ".png";
                 backName = folder + File.separatorChar + "back.png";
                 this.addCard(fileName, backName);
             }catch(FileNotFoundException ex)
             {
+                //Tell the console line how many cards were read in if there was a problem
                 System.out.println("Done reading cards at " + i);
                 System.out.println("File Name: " + fileName);
                 throw ex;
